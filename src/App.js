d8f389e99f8539c8ea2cc2448e68c6d811a37f90
jean5065 <jean5065@adcrow.tech>
2021-11-12 18:26:24 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Outlet, Link } from "react-router-dom";
+import ScrollGenerator from './components/ScrollGenerator/ScrollGenerator';
+import styles from './App.module.scss';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    return (
+      <div className={styles.app}>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="routes/invoices">Invoices</Link> |{" "}
+        <Link to="routes/ScrollGenerator">ScrollGenerator</Link> |{" "}
+      </nav>
+      <Outlet />
     </div>
-  );
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollGenerator from "./components/ScrollGenerator/ScrollGenerator";
+import Invoices from "./components/Invoices";
+import Invoice from "./components/Invoice";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/routes/scrollgenerator" element={<ScrollGenerator />} />
+            <Route path="/routes/invoices" element={<Invoices />}>
+              <Route
+                index
+                element={
+                  <main style={{ padding: "1rem" }}>
+                    <p>Select an invoice</p>
+                  </main>
+                }
+              />
+              <Route path=":invoiceId" element={<Invoice />} />
+            </Route>
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
