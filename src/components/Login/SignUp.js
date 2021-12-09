fb0461988f19cf820217b9c35270445f5f17ab62
jean5065 <jean5065@adcrow.tech>
2021-12-09 09:33:36 +0800

    fix: add validation

--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { addMember } from 'reducers/member'
 
@@ -6,6 +6,7 @@ function SignUp() {
     const dispatch = useDispatch()
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [valid, setValid] = useState(false)
 
     const handleOnSubmit = (e) => {
         e.preventDefault()
@@ -14,16 +15,25 @@ function SignUp() {
         setPassword('')
     }
 
+    useEffect(() => {
+        setValid(username && password)
+    }, [username, password])
+
     return (
         <div>
             <form onSubmit={handleOnSubmit}>
                 <label htmlFor="username">username</label>
                 <br />
                 <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete />
+                {valid && <p>Valid</p>}
+                {!valid && <p>Invalid</p>}
+
                 <br />
                 <label htmlFor="password" >password</label>
                 <br />
                 <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete />
+                {valid && <p>Valid</p>}
+                {!valid && <p>Invalid</p>}
                 <br />
                 <br />
                 <button type="submit">Submit</button>
